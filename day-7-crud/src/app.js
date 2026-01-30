fd41c49b2b9ffeafac2deddedf329c24b66412fb
Sujoy Saha <ace.sujoy@gmail.com>
2026-01-30 16:27:18 +0530

    Post Nd Get api

--- day-7-crud/src/app.js
+++ day-7-crud/src/app.js
@@ -20,4 +20,14 @@ app.post("/notes", async (req, res) => {
     });
 });
 
+
+app.get('/notes',async(req,res)=>{
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        massage:"Note Fatched SuccessFully",
+        notes
+    })
+})
+
 module.exports = app;
