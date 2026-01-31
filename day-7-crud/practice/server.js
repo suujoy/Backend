6c9bd3a5fbfdb79e7694113ae17c1093a7d69751
Sujoy Saha <ace.sujoy@gmail.com>
2026-01-31 17:25:45 +0530

    Delete day-7-crud/practice directory

--- day-7-crud/practice/server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./src/app");
-const mongoose  = require('mongoose')
-const connectToDb = require('./src/config/database')
-
-connectToDb()
-
-app.listen(3000, () => {
-    console.log("Server is running on port no 3000");
-});
