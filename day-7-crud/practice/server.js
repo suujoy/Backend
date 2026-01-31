026cf61b1977d24e7fd5ceb1e3950810aa0f5a7b
Sujoy Saha <ace.sujoy@gmail.com>
2026-01-31 17:39:33 +0530

    Database

--- day-7-crud/practice/server.js
+++ day-7-crud/practice/server.js
@@ -1,8 +1,10 @@
-const app = require("./src/app");
-const mongoose  = require('mongoose')
-const connectToDb = require('./src/config/database')
+require("dotenv").config();
 
-connectToDb()
+const app = require("./src/app");
+const mongoose = require("mongoose");
+const connectToDb = require("./src/config/database");
+
+connectToDb();
 
 app.listen(3000, () => {
     console.log("Server is running on port no 3000");
--- day-7-crud/practice/src/config/database.js
+++ day-7-crud/practice/src/config/database.js
@@ -1,13 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const connectToDb = () => {
-    mongoose
-        .connect(
-            `mongodb+srv://sujoy:ZqVNdwxkGf9OLJug@cluster0.v2bdxrd.mongodb.net/-day-7-practice`,
-        )
-        .then(() => {
-            console.log("Connected to Datebase");
-        });
-};
+const connectToDb = ()=>{
+    mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log('Connected to Database')
+    })
+}
 
-module.exports = connectToDb;
+module.exports= connectToDb
