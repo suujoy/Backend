34cb7704bce9ae20bdda5ec44c611f5cedfae8ca
Sujoy Saha <ace.sujoy@gmail.com>
2026-02-10 12:26:27 +0530

    10 feb

--- day-10-deployment/Backend/server.js
+++ day-10-deployment/Backend/server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const connectToDb = require("./src/config/database");
 
 connectToDb();
+console.log('Hello')
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
