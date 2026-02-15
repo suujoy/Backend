af86d175eebfa34cd729c468bfb502a0ff718a8e
Sujoy Saha <ace.sujoy@gmail.com>
2026-02-15 12:40:07 +0530

    Fix Wild Card

--- day-13/Backend/src/app.js
+++ day-13/Backend/src/app.js
@@ -60,8 +60,9 @@ app.patch("/api/notes/:id", async (req, res) => {
     });
 });
 
-app.use("*name", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "/public/index.html"));
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
+
 
 module.exports = app;
