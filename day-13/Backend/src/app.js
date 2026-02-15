93b9396eca98978ada812c21d2fa14de38317313
Sujoy Saha <ace.sujoy@gmail.com>
2026-02-15 14:25:19 +0530

    fix path

--- day-13/Backend/src/app.js
+++ day-13/Backend/src/app.js
@@ -60,7 +60,7 @@ app.patch("/api/notes/:id", async (req, res) => {
     });
 });
 
-app.get("*name", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
