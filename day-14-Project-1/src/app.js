7da95029aeda0fd5399a9adad885c1430cfb2a40
Sujoy Saha <ace.sujoy@gmail.com>
2026-02-19 19:57:53 +0530

    i need to fix my life

--- day-14-Project-1/src/app.js
+++ day-14-Project-1/src/app.js
@@ -4,6 +4,7 @@ const postRouter = require("./routes/post.routes");
 const cookieParser = require("cookie-parser");
 
 const app = express();
+console.log('sorry')
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRouter);
