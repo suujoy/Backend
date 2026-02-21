24a3f5d8d6b0be117f5efc3aef1aac562574b8d6
Sujoy Saha <ace.sujoy@gmail.com>
2026-02-21 17:30:12 +0530

    user featcherr

--- day-14-insta-clone/src/controller/post.controller.js
+++ day-14-insta-clone/src/controller/post.controller.js
@@ -16,24 +16,98 @@ const createPostController = async (req, res) => {
         });
     }
 
-    const decoded = jwt.verify(token,process.env.JWT_SECRET)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     const file = await imageKit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), "file"),
         fileName: "test",
-        folder:'insta-clone-post'
+        folder: "insta-clone-post",
     });
 
     const post = await postModel.create({
-        caption:req.body.caption,
-        imgUrl:file.url,
-        user:decoded.id
-    })
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user: decoded.id,
+    });
 
     res.status(201).json({
-        message:'post created successfully',
-        post
-    })
+        message: "post created successfully",
+        post,
+    });
 };
 
-module.exports = { createPostController };
+const getPostController = async (req, res) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Token not found , Unauthorized access",
+        });
+    }
+
+    let decoded = null;
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+        return res.status(401).json({
+            message: "Token not found , Unauthorized access",
+        });
+    }
+
+    const userId = decoded.id;
+
+    const posts = await postModel.find({
+        user: userId,
+    });
+
+    res.status(200).json({
+        message: "Posts fetched successfully",
+        posts,
+    });
+};
+
+const getPostDetails = async (req, res) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Token not found , Unauthorized access",
+        });
+    }
+
+    let decoded;
+
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+        return res.status(401).json({
+            message: "Invalid Token",
+        });
+    }
+
+    const userId = decoded.id;
+    const postId = req.params.postId;
+
+    const post = await postModel.findById(postId);
+
+    if (!post) {
+        return res.status(404).json({
+            message: "post not found",
+        });
+    }
+
+    const isValidUser = post.user.toString() === userId;
+
+    if (!isValidUser) {
+        return res.status(403).json({
+            message: "Forbidden  Content",
+        });
+    }
+
+    res.status(200).json({
+        message: "Post fetched successfully",
+        post,
+    });
+};
+
+module.exports = { createPostController, getPostController,getPostDetails };
--- day-14-insta-clone/src/routes/post.routes.js
+++ day-14-insta-clone/src/routes/post.routes.js
@@ -1,10 +1,29 @@
 const express = require("express");
-const { createPostController } = require("../controller/post.controller");
+const { createPostController, getPostController, getPostDetails } = require("../controller/post.controller");
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 
 const postRouter = express.Router();
 
+
+/**
+ * POST /api/post
+ */
 postRouter.post("/",upload.single('image'), createPostController);
+
+
+/**
+ * GET /api/post
+ */
+postRouter.get('/',getPostController)
+
+
+/**
+ * GET /api/post/details/:postId
+ * -return an detail about specific post with the id also check wheather the post belongs to the user that is recuest came from
+ */
+
+postRouter.get('/details/:postId',getPostDetails)
+
 
 module.exports = postRouter;
