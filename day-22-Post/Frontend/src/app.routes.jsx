9218025b0e8630be986b0891df4970ed0fdda345
Sujoy Saha <ace.sujoy@gmail.com>
2026-02-27 23:42:31 +0530

    post ui complete

--- day-22-Post/Frontend/src/app.routes.jsx
+++ day-22-Post/Frontend/src/app.routes.jsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
+import Feed from "./features/posts/pages/Feed";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <h1>This is home page</h1>,
+        element: <Feed />,
     },
     {
         path: "/login",
--- day-22-Post/Frontend/src/features/posts/pages/Feed.jsx
+++ day-22-Post/Frontend/src/features/posts/pages/Feed.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import "../styles/feed.scss";
 
 const Feed = () => {
-  return (
-    <main className="feed-page">
-        
-    </main>
-  )
-}
+    return (
+        <main className="feed-page">
+            <div className="posts">
+                {/**
+                 * Single Post
+                 */}
+                <div className="post">
+                    <div className="user">
+                        <div className="img-wrapper">
+                            <img
+                                src="https://images.unsplash.com/photo-1770954179366-7a85687c1b64?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxN3x8fGVufDB8fHx8fA%3D%3D"
+                                alt=""
+                            />
+                        </div>
+                        <div className="username">username</div>
+                    </div>
 
-export default Feed
+                    <img
+                        src="https://images.unsplash.com/photo-1772173333598-31ffc020d58a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNHx8fGVufDB8fHx8fA%3D%3D"
+                        alt=""
+                    />
+
+                    <div className="icons">
+                        <div className="left"></div>
+                        <div className="right"></div>
+                    </div>
+
+                    <div className="bottom">username_username</div>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default Feed;
