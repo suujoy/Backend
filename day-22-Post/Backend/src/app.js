e1251b2874a476617cc092c1bc73b9bb72ff025a
Sujoy Saha <ace.sujoy@gmail.com>
2026-02-28 21:12:31 +0530

    deployee

--- day-22-Post/Backend/src/app.js
+++ day-22-Post/Backend/src/app.js
@@ -2,13 +2,16 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    credentials:true,
-    origin:'http://localhost:5173'
-}));
+app.use(
+    cors({
+        credentials: true,
+        origin: "http://localhost:5173",
+    }),
+);
 
 /**
  * Require Routes
@@ -20,8 +23,13 @@ const userRouter = require("./routes/user.routes");
 /**
  * Using Routes
  */
+
 app.use("/api/auth", authRouter);
 app.use("/api/post", postRouter);
 app.use("/api/user", userRouter);
+app.use(express.static('./public'))
+app.use("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 module.exports = app;
