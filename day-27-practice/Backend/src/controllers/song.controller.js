a031bef16e0db7495046a79b6095f9c50d1593e6
Sujoy Saha <ace.sujoy@gmail.com>
2026-03-05 08:44:30 +0530

    Backend done for moodify

--- day-27-practice/Backend/src/controllers/song.controller.js
+++ day-27-practice/Backend/src/controllers/song.controller.js
@@ -8,17 +8,30 @@ const uploadSongController = async (req, res) => {
 
     const tags = id3.read(songBuffer);
 
-    const songFile = await uploadFile({
-        buffer: songBuffer,
-        filename: tags.title + "mp3",
-        folder: "/moodify/songs",
-    });
+    // const songFile = await uploadFile({
+    //     buffer: songBuffer,
+    //     filename: tags.title + "mp3",
+    //     folder: "/moodify/songs",
+    // });
 
-    const posterFile = await uploadFile({
-        buffer: tags.image.imageBuffer,
-        filename: tags.title + "jpeg",
-        folder: "/moodify/posters",
-    });
+    // const posterFile = await uploadFile({
+    //     buffer: tags.image.imageBuffer,
+    //     filename: tags.title + "jpeg",
+    //     folder: "/moodify/posters",
+    // });
+
+    const [songFile, posterFile] = await Promise.all([
+        uploadFile({
+            buffer: songBuffer,
+            filename: tags.title + ".mp3",
+            folder: "/moodify/songs",
+        }),
+        uploadFile({
+            buffer: tags.image.imageBuffer,
+            filename: tags.title + ".jpeg",
+            folder: "/moodify/posters",
+        }),
+    ]);
 
     const song = await songModel.create({
         url: songFile.url,
@@ -33,4 +46,15 @@ const uploadSongController = async (req, res) => {
     });
 };
 
-module.exports = { uploadSongController };
+const getSongController = async (req, res) => {
+    const { mood } = req.query;
+
+    const song = await songModel.findOne({ mood });
+
+    res.status(200).json({
+        message: "Song Fetched Successfully",
+        song,
+    });
+};
+
+module.exports = { uploadSongController,getSongController };
--- day-27-practice/Backend/src/routes/song.route.js
+++ day-27-practice/Backend/src/routes/song.route.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const { upload } = require("../middlewares/upload.middleware");
-const { uploadSongController } = require("../controllers/song.controller");
+const {
+    uploadSongController,
+    getSongController,
+} = require("../controllers/song.controller");
 
 const songRouter = express.Router();
 
 songRouter.post("/", upload.single("song"), uploadSongController);
+
+songRouter.get("/", getSongController);
 
 module.exports = songRouter;
