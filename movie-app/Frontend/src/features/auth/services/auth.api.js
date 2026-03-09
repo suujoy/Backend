d84834380b750946f380d0e3658764e2e61a35d2
Sujoy Saha <ace.sujoy@gmail.com>
2026-03-09 17:48:05 +0530

    url fix

--- movie-app/Frontend/src/features/auth/services/auth.api.js
+++ movie-app/Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/auth",
+    baseURL: "https://movieverse-25l8.onrender.com/api/auth",
     withCredentials: true,
 });
 
