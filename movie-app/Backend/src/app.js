841515d748eaf5ef648d9aa38d78eedc37bbaaf6
Sujoy Saha <ace.sujoy@gmail.com>
2026-03-09 17:27:52 +0530

    fix changes

--- movie-app/Backend/src/app.js
+++ movie-app/Backend/src/app.js
@@ -2,18 +2,26 @@ const express = require("express");
 const authRouter = require("./routes/auth.route");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
+
 app.use(
     cors({
         origin: "http://localhost:5173",
         credentials: true,
     }),
 );
+app.use(express.static('./public'))
+
 
 app.use("/api/auth", authRouter);
+
+app.use("*name", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 module.exports = app;
